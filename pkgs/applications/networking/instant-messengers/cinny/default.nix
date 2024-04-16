4a113bde0125638bb00dd784f5eee2d43613c3c4
Qyriad <qyriad@qyriad.me>
2024-04-15 21:18:25 -0600

    Revert "cinny: convert to a split derivation"
    This reverts commit 558d901f11f7678821590d9cf8028660f5f2ae56.

--- pkgs/applications/networking/instant-messengers/cinny/default.nix
+++ pkgs/applications/networking/instant-messengers/cinny/default.nix
@@ -8,21 +8,17 @@
 , pixman
 , cairo
 , pango
-, stdenvNoCC
-, nodejs
+, stdenv
 , darwin
 , conf ? { }
 }:
 
 let
   configOverrides = writeText "cinny-config-overrides.json" (builtins.toJSON conf);
-  inherit (nodejs.pkgs) node-gyp;
 in
 buildNpmPackage rec {
   pname = "cinny";
   version = "3.1.0";
-
-  outputs = [ "out" "dev" ];
 
   src = fetchFromGitHub {
     owner = "cinnyapp";
@@ -31,49 +27,35 @@ buildNpmPackage rec {
     hash = "sha256-GcygxK9NcGlv4rwxQCJqi0BhNlOTFxjGB8mbfTaBMOk=";
   };
 
-  patches = [
-    ./vite-svg-no-sourcemap.patch
-  ];
-
   npmDepsHash = "sha256-4R+To2LhcnEM9x1noo6MhCckyBKgPWiAi7zgDqAmaN0=";
-
-  dontNpmPrune = true;
 
   nativeBuildInputs = [
     jq
     python3
     pkg-config
-    nodejs
-    node-gyp
   ];
 
   buildInputs = [
     pixman
     cairo
     pango
-  ] ++ lib.optionals stdenvNoCC.isDarwin [
+  ] ++ lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.CoreText
   ];
 
-  npmRebuildFlags = [
-    "--ignore-scripts"
-  ];
+  installPhase = ''
+    runHook preInstall
 
-  postInstall = ''
-    # npmInstallHook automatically places things in $out, which is where we ant
-    # Vite's artifacts to go.
-    # Conversely, we want npm's artifacts to go in #dev, so:
-    mv -v "$out" "$dev"
-    cp -r ./dist "$out"
-    # Using the npm artifacts is also always going to require the web artifacts too,
-    # so let's symlink the web artifacts under $dev.
-    ln -sv "$out" "$dev/lib/node_modules/cinny/dist"
+    cp -r dist $out
+    jq -s '.[0] * .[1]' "config.json" "${configOverrides}" > "$out/config.json"
+
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "Yet another Matrix client for the web";
     homepage = "https://cinny.in/";
-    maintainers = with maintainers; [ abbe ashkitten qyriad ];
+    maintainers = with maintainers; [ abbe ashkitten ];
     license = licenses.agpl3Only;
     platforms = platforms.all;
   };

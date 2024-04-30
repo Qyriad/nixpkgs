b6ddb65a9699ec7f8d410dfb4f68983db0597276
Qyriad <qyriad@qyriad.me>
2024-04-30 13:59:47 -0600

    cdecl: refactor, fix Darwin build, update
    I've also taken ownership of this package, de-orphening it.

--- pkgs/development/tools/cdecl/default.nix
+++ pkgs/development/tools/cdecl/default.nix
@@ -1,26 +1,67 @@
-{lib, stdenv, fetchurl, bison, flex, readline, ncurses, gnused}:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  bison,
+  flex,
+  readline,
+  ncurses,
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "cdecl";
   version = "2.5";
-  src = fetchurl {
-    url = "https://www.cdecl.org/files/${pname}-blocks-${version}.tar.gz";
-    sha256 = "1b7k0ra30hh8mg8fqv0f0yzkaac6lfg6n376drgbpxg4wwml1rly";
+
+  src = fetchFromGitHub {
+    owner = "ridiculousfish";
+    repo = "cdecl-blocks";
+    # Latest commit as of 2024/04/30. Repo does not have any tags,
+    # but the readme says version 2.5
+    rev = "fe9747fb3280576415c9605c350a48091699fbfb";
+    hash = "sha256-p6L/67/q/X7hOftVr9a5eQOn4UYe2ZqER7aZ6WoGFp8=";
   };
 
+  __structuredAttrs = true;
+  stripDeps = true;
+
   patches = [ ./cdecl-2.5-lex.patch ];
-  preBuild = ''
-    ${gnused}/bin/sed 's/getline/cdecl_getline/g' -i cdecl.c;
-    makeFlagsArray=(CFLAGS="-DBSD -DUSE_READLINE -std=gnu89" LIBS=-lreadline);
-    makeFlags="$makeFlags PREFIX=$out BINDIR=$out/bin MANDIR=$out/man1 CATDIR=$out/cat1 CC=$CC";
-    mkdir -p $out/bin;
+
+  postPatch = ''
+    substituteInPlace cdecl.c \
+      --replace-fail "getline" "cdecl_getline"
   '';
-  buildInputs = [bison flex readline ncurses];
+
+  nativeBuildInputs = [ flex ];
+
+  buildInputs = [
+    bison
+    readline
+    ncurses
+  ];
+
+  buildFlags = [
+    # Makefile hardcodes `gcc`.
+    "CC=${lib.getExe stdenv.cc}"
+    # "-DBSD" indicates compiling for a Unix system.
+    "CFLAGS=-DBSD -DUSE_READLINE -std=gnu89 -Wno-error=parentheses -Wno-error=incompatible-pointer-types -Wno-error-int-conversion"
+    "LIBS=-lreadline"
+  ];
+
+  makeFlags = [
+    "PREFIX=${placeholder "out"}"
+    "BINDIR=${placeholder "out"}/bin"
+    "MANDIR=${placeholder "out"}/man1"
+    "CATDIR=${placeholder "out"}/cat1"
+  ];
+
+  preInstall = ''
+    mkdir -p "$out/bin"
+  '';
 
   meta = {
     description = "Translator English -- C/C++ declarations";
     license = lib.licenses.publicDomain;
-    maintainers = with lib.maintainers; [ ];
-    platforms = lib.platforms.unix;
+    maintainers = with lib.maintainers; [ qyriad ];
+    platforms = with lib.platforms; unix;
   };
-}
+})

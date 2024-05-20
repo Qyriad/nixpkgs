fc739a7e28d2c1e5e01a2fc305afa53d4f7ff999
Qyriad <qyriad@qyriad.me>
2024-05-19 22:23:19 -0600

    init

--- pkgs/build-support/php/builders/default.nix
+++
@@ -1,8 +0,0 @@
-{ callPackage, callPackages, ... }:
-{
-  v1 = {
-    buildComposerProject = callPackage ./v1/build-composer-project.nix { };
-    mkComposerRepository = callPackage ./v1/build-composer-repository.nix { };
-    composerHooks = callPackages ./v1/hooks { };
-  };
-}
